d0654a3398f96e69cd53998bbfcf5c85e0db9181
Maciej Ciemborowicz <maciej.ciemborowicz@gmail.com>
2019-06-01 02:59:12 +0200

    Add editing settings and basic help.

--- lib/gm3156/device.rb
+++ lib/gm3156/device.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
+require 'pry'
+
 module GM3156
   require 'hidapi'
   require_relative 'errors'
+  require_relative 'settings'
+  require_relative 'record'
 
   class Device
     STATE_REQUEST = [0xb3, Random.rand(256), Random.rand(256), Random.rand(256), 0, 0, 0, 0].pack('C*')
 
-    def initialize(vendor_id = 0x64bd, product_id = 0x74e3)
+    def initialize(vendor_id = 0x64bd, product_id = 0x74e3, **args)
       self.vendor_id = vendor_id
       self.product_id = product_id
-      self.speed = :slow
-    end
-
-    def open
       self.device = HIDAPI.open(vendor_id, product_id)
+      read_current_state
+      set_settings(**args) unless args.empty?
     end
 
     def close
@@ -24,31 +26,39 @@ module GM3156
     def read(&block)
       loop do
         Thread.new do
-          begin
-            block.call(read_current_state)
-          rescue InvalidCaptureDataLengthError
-            sleep 0.1
-            retry
-          end
+          read_current_state(&block)
         end
-        sleep speed
+        sleep settings.speed == :fast? ? 0.5 : 1
       end
     end
 
-    def speed=(speed)
-      raise ArgumentError, 'Only :slow and :fast arguments allowed.' unless %i[slow fast].include?(speed)
+    def set_settings(**args)
+      args.each do |name, value|
+        settings.send("#{name}=", value) if settings.respond_to?("#{name}=")
+      end
+      data = [0x56, settings.pack, 0, 0, 0, 0, 0, 0].pack('C*')
 
-      @speed = speed == :fast ? 0.5 : 1
+      device.write(data)
+      read_cap_data
+      sleep(0.1)
     end
 
     private
 
-    def read_current_state
-      send_state_request
-      data = read_cap_data
-      raise InvalidCaptureDataLengthError.new('Capture data should have 8 bytes.', data) if data.length != 8
-      
-      data.unpack('H*').first
+    def read_current_state(&block)
+      begin
+        send_state_request
+        data = read_cap_data
+
+        raise InvalidCaptureDataLengthError.new('Capture data should have 8 bytes.', data) if data.length != 8
+
+        record = Record.new(data.unpack('H*').first)
+        self.settings = record.settings
+        block_given? ? block.call(record) : record
+      rescue InvalidCaptureDataLengthError
+        sleep 0.1
+        retry
+      end
     end
 
     def send_state_request
@@ -57,13 +67,10 @@ module GM3156
 
     def read_cap_data
       cap_data = nil
-
       cap_data = device.read_timeout(1000) until cap_data && !cap_data.empty?
-
       cap_data
     end
 
-    attr_accessor :vendor_id, :product_id, :device
-    attr_reader :speed
+    attr_accessor :vendor_id, :product_id, :device, :settings
   end
 end
---
+++ lib/gm3156/settings.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+module GM3156
+  class Settings
+    AVAILABLE_RANGES = [
+      '30-120',
+      '30-60',
+      '50-100',
+      '60-110',
+      '80-130'
+    ]
+
+    def initialize(message)
+      assign_settings(message)
+    end
+
+    def assign_settings(message)
+      binary_settings = message[4].unpack('b4*').first.chars.map { |c| c.to_i == 1 }
+      self.filter = binary_settings[0] ? :c : :a
+      self.max_mode = binary_settings[1]
+      self.speed = binary_settings[2] ? :fast : :slow
+      self.range = message[5].to_i
+    end
+
+    def pack
+      filter_bit = filter == :c ? 1 : 0
+      max_mode_bit = max_mode ? 1 : 0
+      speed_bit = speed == :fast ? 1 : 0
+
+      boolean_options = "0#{speed_bit}#{max_mode_bit}#{filter_bit}".to_i(2)
+      "#{boolean_options}#{range}".to_i(16)
+    end
+
+    attr_accessor :filter, :max_mode, :speed, :range
+
+  end
+end
